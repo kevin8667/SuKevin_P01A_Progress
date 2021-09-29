d0ab04bb63ee9007d1db7ba600be66568ef0ea41
KevinSu <kevin86677372@yahoo.com.tw>
2021-09-29 05:15:17 -0500

    Final

--- Assets/Scripts/ShieldHealth.cs
+++ Assets/Scripts/ShieldHealth.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class ShieldHealth : Health , IDamageable
 {
+
+    [SerializeField] Material _objectMaterial;
+    Color _objectColor;
     private GameObject _boss;
 
     private void Awake()
     {
         _boss = GameObject.FindGameObjectWithTag("Boss");
         _currentHealth = _maxHealth;
+        _objectColor = _objectMaterial.color;
 
     }
+
+
 
 
     public override void TakeDamage(int amount)
@@ -22,8 +28,20 @@ public class ShieldHealth : Health , IDamageable
         {
             AudioHelper.PlayClip2D(_damagedSound, 1f);
         }
+
+        if (_currentHealth == 2) 
+        {
+            _objectMaterial.color = new Color(0.5f, 0.3f, 0.4f, 0.3843f);
+        }
+
+        if (_currentHealth == 1) 
+        {
+            _objectMaterial.color = new Color(1f, 0.1f, 0.2f, 0.3843f);
+
+        }
         if (_currentHealth <= 0)
         {
+            _objectMaterial.color = _objectColor;
             Kill();
             Boss _newBoss = _boss.gameObject.GetComponent<Boss>();
             _newBoss._isShielded = false;
